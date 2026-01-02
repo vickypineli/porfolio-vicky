d22d8dcd2dbf09a2111f7999539638a94edc7dd0
Vicky Pineli <v.pinero70@gmail.com>
2026-01-02 20:27:50 +0100

    add image and texts in diferent languajes

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+// src/pages/AboutPage/AboutPage.jsx
+import { useTranslation } from "react-i18next";
+import styles from "./AboutPage.module.scss";
+import womanKoder from "../../assets/images/womanKoder.png";
 
 export default function AboutPage() {
-    return <h1>AboutPage</h1>;
-}   
+  const { t } = useTranslation("about");
+
+  const hobbies = t("hobbies_list", { returnObjects: true });
+
+  return (
+    <section className={styles.about}>
+      <div className={styles.container}>
+        
+        {/* TEXTO */}
+        <div className={styles.text}>
+          <h1 className={styles.title}>{t("title")}</h1>
+
+          <p>{t("name")}</p>
+          <p>{t("evolution")}</p>
+          <p>{t("profile")}</p>
+          <p>{t("current")}</p>
+
+          <h2 className={styles.subtitle}>{t("hobbies_title")}</h2>
+
+          <ul className={styles.hobbies}>
+            {hobbies.map((hobby, index) => (
+              <li key={index}>{hobby}</li>
+            ))}
+          </ul>
+        </div>
+
+        {/* FOTO (placeholder) */}
+        <div className={styles.photoWrapper}>
+        <img
+            src={womanKoder}
+            alt="woman developer illustration"
+            className={styles.photo}
+        />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,12 +1,19 @@
 // src/pages/ProjectsPage/ProjectsPage.jsx
 import { projects } from '../../data/projects';
+import { useTranslation } from "react-i18next";
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import styles from './ProjectsPage.module.scss';
 
 export default function ProjectsPage() {
+  const { t } = useTranslation("projects");
   return (
     <section className={styles.page}>
-      <h2 className={styles.title}>Projects</h2>
+      <h2 className={styles.title}>
+        {t("title")}
+        </h2>
+        <p>{t("description")}
+          <br />
+        </p>
 
       <div className={styles.grid}>
         {projects.map((project) => (
