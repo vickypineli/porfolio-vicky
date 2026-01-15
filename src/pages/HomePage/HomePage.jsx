9e48a9d199e712a9f30f0272815eab0b8d1ee921
Vicky Pineli <v.pinero70@gmail.com>
2026-01-15 11:54:17 +0100

    chages in homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,32 +1,20 @@
 //src/pages/HomePage/HomePage.jsx
-import HeroSection from "../HomePage/sections/HeroSection/HeroSection";
-import AboutPreviewSection from "../HomePage/sections/AboutPreviewSection/AboutPreviewSection";
-import ProjectsPreviewSection from "../HomePage/sections/ProjectsPreviewSection/ProjectsPreviewSection";
-import ContactPreviewSection from "../HomePage/sections/ContactPreviewSection/ContactPreviewSection";
-import ParallaxSection from "../../components/Parallax/ParallaxSection";
+
+import HeroSection from "./sections/HeroSection/HeroSection";
+import AboutSection from "./sections/AboutSection/AboutSection";
+import ProjectsSection from "./sections/ProjectsPreviewSection/ProjectsSection";
+import ContactSection from "./sections/ContactSection/ContactSection";
+
 
 //import styles from "./HomePage.module.scss";
-
-
-
-
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
-
-      <ParallaxSection speed={0.45}>
-        <AboutPreviewSection />
-      </ParallaxSection>
-
-      {/* <ParallaxSection speed={0.6}>
-        <ProjectsPreviewSection />
-      </ParallaxSection> */}
-
-      <ParallaxSection speed={0.25}>
-        <ContactPreviewSection />
-      </ParallaxSection>
+      <AboutSection />
+      <ProjectsSection />  
+      <ContactSection />
     </main>
   );
 }
---
+++ src/pages/HomePage/sections/ContactSection/ContactSection.jsx
@@ -0,0 +1,74 @@
+//src/pages/HomePage/sections/ContactPreviewSection/ContactPreviewSection.jsx
+
+
+import { useEffect, useRef } from "react";
+import { useTranslation, Trans } from "react-i18next";
+import { Link } from "react-router-dom";
+import styles from "./ContactSection.module.scss";
+
+export default function ContactSection() {
+  const { t } = useTranslation("homeContact");
+
+  const vantaRef = useRef(null);
+  const vantaEffect = useRef(null);
+
+  useEffect(() => {
+    if (!window.VANTA?.GLOBE || !window.THREE) return;
+
+    if (!vantaEffect.current && vantaRef.current) {
+      vantaEffect.current = window.VANTA.GLOBE({
+        el: vantaRef.current,
+        THREE: window.THREE,
+      // Controles de la animacion
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: true,
+      //Tamaño de la animacion
+        minHeight: 500.00,
+        minWidth: 500.00,
+      //tamaño y escala del globo
+        scale: 1.00,
+        scaleMobile: 1.00,
+        size:0.90,
+      //colores
+        color2: 0xe9e5eb,
+        color: 0xff3f81,
+        backgroundAlpha: 0 ,
+      });
+    }
+
+    return () => {
+      vantaEffect.current?.destroy();
+      vantaEffect.current = null;
+    };
+  }, []);
+
+  return (
+    <section
+      ref={vantaRef}
+      className={styles.section}
+      aria-labelledby="contact-preview-title"
+    >
+      <div className={styles.Inner}>
+        <div className={styles.Content}>
+          <p className={styles.contactSubtitle0}>
+            {t("subtitle0")}
+          </p>
+          <h2 id="contact-preview-title" className={styles.contactTitle}>
+            {t("title")}
+          </h2>
+          <div className={styles.contactSubtitle}>
+            <Trans
+              t={t}
+              i18nKey="subtitle"
+              components={{ span: <span className={styles.contactSpan} /> }}
+            />
+          </div>
+          <Link to="/contact" className={styles.contactButton}>
+            {t("button")}
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
